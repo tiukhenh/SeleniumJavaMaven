90225fb2e73128db651501a0ecc7405939ae6e58
ongtukhanh <khanhongmx@gmail.com>
2024-04-25 10:34:03 +0700

    update config cucumber report

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -2,15 +2,19 @@ package runner;
 
 
 import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
 import org.junit.runner.RunWith;
+import org.testng.annotations.Test;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = "stepDefinition",
         monochrome = true,
-        plugin= {"pretty", "json:target/cucumber-reports/cucumber.json","html:target/cucumber-html-report"}
+        plugin= {"pretty", "html:target/cucumber-reports/TestRunner.html",
+                "json:target/cucumber-reports/TestRunner.json"}
 )
-public class TestRunner {
+@Test
+public class TestRunner extends AbstractTestNGCucumberTests {
 }
